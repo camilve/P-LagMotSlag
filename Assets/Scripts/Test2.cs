944d9b1e6d3f4eae865f64436b9deb65b0e8bf79
Unknown <camilve@stud.ntnu.no>
2019-03-21 13:41:18 +0100

    Humanoid is moving

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Windows.Kinect;
 using Kinect = Windows.Kinect;
 
 public class Test2 : MonoBehaviour {
@@ -64,18 +65,25 @@ public class Test2 : MonoBehaviour {
      }*/
 
     protected Animator animator;
-    //private GameObject player;
+    private GameObject player;
     public GameObject BodySourceManager;
     private BodySourceManager _BodyManager;
 
 
     void Start ()
     {
-        //player = gameObject;
-        //animator = player.GetComponent<Animator>();
-        animator = GetComponent<Animator>();
+        player = gameObject;
+        animator = player.GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         Debug.Log(animator);
     }
+
+    void Update()
+    {
+        
+    }
+
+
     //a callback for calculating IK
     void OnAnimatorIK()
     {
@@ -107,35 +115,83 @@ public class Test2 : MonoBehaviour {
             }
             if (body.IsTracked)
             {
-                var pos = body.Joints[Windows.Kinect.JointType.FootLeft].Position;
-                var pos2 = body.Joints[Windows.Kinect.JointType.KneeLeft].Position;
-                Debug.Log("pos "+ pos.X +" "+ pos.Y +" "+ pos.Z);
-                Debug.Log("pos2 " + pos2.X + " " + pos2.Y + " " + pos2.Z);
-                if (animator)
+                //Place the gameobject with the spine as the centre.
+                player.transform.position = GetVector(body.Joints[Kinect.JointType.SpineBase].Position);
+
+                //Place the feet the same place as the Kinect.            
+#warning Vet ikke om foten går frem eller tilbake
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+                animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKHintPositionWeight(AvatarIKHint.LeftKnee, 1);
+                animator.SetIKHintPositionWeight(AvatarIKHint.RightKnee, 1);
+                animator.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, 1);
+                animator.SetIKHintPositionWeight(AvatarIKHint.RightElbow, 1);
+
+
+                animator.SetIKPosition(AvatarIKGoal.LeftFoot, GetVector(body.Joints[Windows.Kinect.JointType.FootLeft].Position));
+                //animator.SetIKRotation(AvatarIKGoal.LeftFoot, new Quaternion(body.Joints[Windows.Kinect.JointType.FootLeft].Position.X, body.Joints[Windows.Kinect.JointType.FootLeft].Position.Y, body.Joints[Windows.Kinect.JointType.FootLeft].Position.Z));
+                animator.SetIKPosition(AvatarIKGoal.RightFoot, GetVector(body.Joints[Windows.Kinect.JointType.FootRight].Position));
+
+                animator.SetIKPosition(AvatarIKGoal.LeftHand, GetVector(body.Joints[Windows.Kinect.JointType.HandLeft].Position));
+                animator.SetIKPosition(AvatarIKGoal.RightHand, GetVector(body.Joints[Windows.Kinect.JointType.HandRight].Position));
+
+                animator.SetIKHintPosition(AvatarIKHint.LeftKnee, GetVector(body.Joints[Windows.Kinect.JointType.KneeLeft].Position));
+                animator.SetIKHintPosition(AvatarIKHint.RightKnee, GetVector(body.Joints[Windows.Kinect.JointType.KneeRight].Position));
+
+                animator.SetIKHintPosition(AvatarIKHint.LeftElbow, GetVector(body.Joints[Windows.Kinect.JointType.ElbowLeft].Position));
+                animator.SetIKHintPosition(AvatarIKHint.RightElbow, GetVector(body.Joints[Windows.Kinect.JointType.ElbowRight].Position));
+
+                //animator.SetLookAtPosition(new Vector3(body.Joints[Windows.Kinect.JointType.Head].Position.X, body.Joints[Windows.Kinect.JointType.Head].Position.Y, body.Joints[Windows.Kinect.JointType.Head].Position.Z));
+
+                /*
+                if (GetVector(body.Joints[Kinect.JointType.ShoulderLeft].Position) != Vector3.zero && GetVector(body.Joints[Windows.Kinect.JointType.ShoulderRight].Position) != Vector3.zero)
                 {
-                    Debug.Log("TEST");
-                    animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
-                    animator.SetIKPosition(AvatarIKGoal.LeftFoot, new Vector3(pos.X * 10, pos.Y * 10, pos.Z * 10));
-                    animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 0);
+                    float _leftShoulderX = body.Joints[Kinect.JointType.ShoulderLeft].Position.X;
+                    float _leftShoulderY = body.Joints[Kinect.JointType.ShoulderLeft].Position.Y;
+                    float _leftShoulderZ = body.Joints[Kinect.JointType.ShoulderLeft].Position.Z;
+                    float _rightShoulderX = body.Joints[Kinect.JointType.ShoulderRight].Position.X;
+                    float _rightShoulderY = body.Joints[Kinect.JointType.ShoulderRight].Position.Y;
+                    float _rightShoulderZ = body.Joints[Kinect.JointType.ShoulderRight].Position.Z;
 
-                    animator.SetIKHintPosition(AvatarIKHint.LeftKnee, new Vector3(pos2.X * 10, pos2.Y * 10, pos2.Z * 10));
+                    float _absoluteX = Mathf.Abs(_leftShoulderX) + Mathf.Abs(_rightShoulderX);
+                    float _absoluteZ = Mathf.Abs(_leftShoulderZ - _rightShoulderZ);
+
+                    float _division = _absoluteX / _absoluteZ;
+                    float _spineAngleRadians = Mathf.Atan(_division);
+
+                    if (_leftShoulderZ > _rightShoulderZ)
+                    {
+                        _spineAngle = 90 + (_spineAngleRadians * (180.0f / Mathf.PI));
+                    }
+                    else
+                    {
+                        _spineAngle = 270 - (_spineAngleRadians * (180.0f / Mathf.PI));
+                    }
                 }
-
-
-
-                
-               // var orientation = body.JointOrientations[Windows.Kinect.JointType.HandRight].Orientation;
-                //animator.SetBoneLocalRotation(HumanBodyBones.RightHand, new Quaternion(orientation.X, orientation.Y, orientation.Z, orientation.W));
-
-                //this.gameObject.transform.position = new Vector3(pos.X * 10, pos.Y * 10, pos.Z * 10);
-                //this.gameObject.transform.rotation = new Quaternion(orientation.X, orientation.Y, orientation.Z, orientation.W);
-                //break;
+                _player.transform.rotation = Quaternion.AngleAxis(-_spineAngle, Vector3.up);
+                */
             }
         }
 
 
 
         
+    }
+    /* Try to make the code better with this...
+    private int getBodypartNr(string bodypart)
+    {
+        int nr = -1;
+        nr = Windows.Kinect.JointType. bodypart
+
+        Vector3 position = new Vector3(, body.Joints[Windows.Kinect.JointType.FootLeft].Position.Y, body.Joints[Windows.Kinect.JointType.FootLeft].Position.Z);    
+    }
+    */
+
+    private Vector3 GetVector(CameraSpacePoint point)
+    {
+        return new Vector3(point.X, point.Y, point.Z);
     }
 
 }
