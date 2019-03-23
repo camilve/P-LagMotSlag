08b0ee93e808441c027b066d59b9effb733a36d5
Unknown <camilve@stud.ntnu.no>
2019-03-23 12:44:27 +0100

    loading page

---
+++ Assets/SwitchScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    static public string loadScene = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GotoBalance1Scene()
+    {
+        loadScene = "Balance 1";
+        SceneManager.LoadScene("PauseScreen");
+    }
+
+    public void GotoBalance2Scene()
+    {
+        loadScene = "Balance 2";
+        SceneManager.LoadScene("Menu");
+    }
+}
