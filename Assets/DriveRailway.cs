3527fd6d335604f734c49b41fb23598d852ffab6
Unknown <camilve@stud.ntnu.no>
2019-03-23 13:57:06 +0100

    Start of balance 2 exercise

---
+++ Assets/DriveRailway.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kinect = Windows.Kinect;
+
+public class DriveRailway : MonoBehaviour
+{
+    public GameObject BodySourceManager;
+    private BodySourceManager _BodyManager;
+    protected GameObject player;
+    public Rigidbody rb; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BodySourceManager == null)
+        {
+            Debug.Log("1");
+            return;
+        }
+
+        _BodyManager = BodySourceManager.GetComponent<BodySourceManager>();
+        if (_BodyManager == null)
+        {
+            Debug.Log("2");
+            return;
+        }
+
+        Kinect.Body[] data = _BodyManager.GetData();
+        if (data == null)
+        {
+            Debug.Log("3 " + _BodyManager);
+            return;
+        }
+
+        foreach (var body in data)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                //Må lagre start posisjon for å vite om man lener seg over!!!
+
+                // Første går det raskere og raskere, kanskje i et høyere level
+                //rb.AddForce(0, 0, 500 * Time.deltaTime);
+                rb.velocity = new Vector3(0, 0, 500 * Time.deltaTime);
+            }
+        }
+    }
+}
