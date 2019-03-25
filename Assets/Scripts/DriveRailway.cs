6e40d0d550025aa56f4321af98f92791b2399f3b
Unknown <camilve@stud.ntnu.no>
2019-03-25 17:48:20 +0100

    Small changes

--- Assets/Scripts/DriveRailway.cs
+++ Assets/Scripts/DriveRailway.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Windows.Kinect;
 using Kinect = Windows.Kinect;
 
 public class DriveRailway : MonoBehaviour
@@ -47,7 +48,17 @@ public class DriveRailway : MonoBehaviour
             }
             if (body.IsTracked)
             {
+
+                Quaternion t = new Quaternion(body.JointOrientations[Windows.Kinect.JointType.SpineShoulder].Orientation.W, body.JointOrientations[Windows.Kinect.JointType.SpineShoulder].Orientation.X, body.JointOrientations[Windows.Kinect.JointType.SpineShoulder].Orientation.Y, body.JointOrientations[Windows.Kinect.JointType.SpineShoulder].Orientation.Z);
+                
                 //Må lagre start posisjon for å vite om man lener seg over!!!
+
+                Vector3 shoulderpos = GetVector(body.Joints[Windows.Kinect.JointType.ShoulderRight].Position);
+                Vector3 footpos = GetVector(body.Joints[Windows.Kinect.JointType.FootRight].Position);
+
+                Debug.Log("shoulder: " + shoulderpos);
+                Debug.Log("foot: " + footpos);
+
 
                 // Første går det raskere og raskere, kanskje i et høyere level
                 //rb.AddForce(0, 0, 500 * Time.deltaTime);
@@ -55,4 +66,10 @@ public class DriveRailway : MonoBehaviour
             }
         }
     }
+    private Vector3 GetVector(CameraSpacePoint point)
+    {
+        return new Vector3(point.X, point.Y, point.Z);
+    }
+
+
 }
